7ec4679457099700fb3488d249bc446bd0823a3e
xjtunzy <3499179467@qq.com>
2025-07-24 18:31:20 +0800

    diffusion model

--- ddpm/ddpm_simple.py
+++ ddpm/ddpm_simple.py
@@ -1,2 +1,11 @@
 import torch
 
+class DDPM():
+
+    def __init__(self,
+                 device,
+                 n_steps: int,
+                 min_beta: float = 0.0001,
+                 max_beta: float = 0.02):
+        betas = torch.linspace(min_beta,max_beta,n_steps).to(device)
+        alphas = 1 - betas
