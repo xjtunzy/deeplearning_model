feb61149b746d1b56b667a591dbbc47561bb4276
xjtunzy <3499179467@qq.com>
2025-07-27 16:19:06 +0800

    ddpm_simple

--- ddpm/ddpm_simple.py
+++ ddpm/ddpm_simple.py
@@ -8,4 +8,52 @@ class DDPM():
                  min_beta: float = 0.0001,
                  max_beta: float = 0.02):
         betas = torch.linspace(min_beta,max_beta,n_steps).to(device)
-        alphas = 1 - betas
+        alphas = 1 - betas  #这个的device属性和betas的一样
+        alpha_bars = torch.empty_like(alphas)
+        product = 1
+        for i,alpha in enumerate(alphas):
+            product *= alpha
+            alpha_bars[i] = product
+        self.betas = betas
+        self.n_steps = n_steps
+        self.alphas = alphas
+        self.alpha_bars = alpha_bars
+
+    def sample_forward(self,x,t,eps=None):
+        alpha_bar = self.alpha_bars[t].reshape(-1,1,1,1)
+        if eps==None:
+            eps = torch.randn_like(x)  #每一个像素通道都是一个标准正态分布噪声的采样
+        res = eps*torch.sqrt(1-alpha_bar)+torch.sqrt(alpha_bar)*x
+        return res
+    
+    def sample_backward(self,img_shape,net,device,simple_var=True):
+        x = torch.randn(img_shape).to(device)
+        net = net.to(device)
+        for t in range(self.n_steps-1,-1,-1):
+            x = self.sample_backward_step(x,t,net,simple_var)
+        return x
+    
+    def sample_backward_step(self, x_t, t, net, simple_var=True):
+
+        n = x_t.shape[0]
+        t_tensor = torch.tensor([t] * n,
+                                dtype=torch.long).to(x_t.device).unsqueeze(1)
+        eps = net(x_t, t_tensor)  #神经网络预测噪声
+
+        if t == 0:
+            noise = 0
+        else:
+            if simple_var:
+                var = self.betas[t]
+            else:
+                var = (1 - self.alpha_bars[t - 1]) / (
+                    1 - self.alpha_bars[t]) * self.betas[t]
+            noise = torch.randn_like(x_t)
+            noise *= torch.sqrt(var)
+
+        mean = (x_t -
+                (1 - self.alphas[t]) / torch.sqrt(1 - self.alpha_bars[t]) *
+                eps) / torch.sqrt(self.alphas[t])
+        x_t = mean + noise
+
+        return x_t
